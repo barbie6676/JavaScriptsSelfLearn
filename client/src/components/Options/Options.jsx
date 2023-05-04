52e5b9ce73a3a2f34501311d8dfaad844698b939
Renxu Hu <rhu@snapchat.com>
2023-05-03 21:02:12 -0700

    upgrade chatbot

--- client/src/components/Options/Options.jsx
+++ client/src/components/Options/Options.jsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 import "./Options.css";
 import ProductCard from "../ProductCard/ProductCard";
 
 const Options = (props) => {
   const sessionId = props.sessionId;
-  const [userMessage, setUserMessage] = useState(undefined);
-  const [products, setProducts] = useState([]);
-  const myRef = useRef(null);
-
-  useEffect(() => {
-    const domNode = myRef.current.previousSibling;
-    if (domNode === undefined || domNode === null) {
-      return;
-    }
-    const reactInternalInstanceKey = Object.keys(domNode).find((key) =>
-      key.startsWith("__reactInternalInstance")
-    );
-    const reactInternalInstance = domNode[reactInternalInstanceKey];
-    const memoizedProps =
-      reactInternalInstance.memoizedProps.children[0]._owner.memoizedProps;
-    setUserMessage(memoizedProps.payload.originalMessage);
-    setProducts(memoizedProps.payload.recommendProducts);
-  }, []);
+  const userMessage = props.payload.originalMessage;
+  const products = props.payload.recommendProducts;
 
   const options = [
     {
@@ -49,7 +33,7 @@ const Options = (props) => {
   ));
 
   return (
-    <div className="results-container" ref={myRef}>
+    <div className="results-container">
       <div className="products-container">{productCards}</div>
       <div className="options-container">{buttonsMarkup}</div>
     </div>
