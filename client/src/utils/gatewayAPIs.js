f7eb30b6581fae15849ff4c545416ef815313c09
Renxu Hu <rhu@snapchat.com>
2023-05-05 17:13:11 -0700

    use prod endpoint

--- client/src/utils/gatewayAPIs.js
+++ client/src/utils/gatewayAPIs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:5000/";
-// const baseURL = "https://stylebot.tryforma.com/server/";
+// const baseURL = "http://127.0.0.1:5000/";
+const baseURL = "https://stylebot.tryforma.com/server/";
 
 const axiosInstance = axios.create({
   baseURL,
